620894ac1a0ebb079bb30b99db641ac0a5655a1d
Rafael Francisco Serrano Urtecho <ik25061@armeriaeskola.eus>
2026-06-15 13:49:55 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Esto expone el frontend a la red local
-    port: 5173  // El puerto por defecto de Vite
+    port: 5172  // El puerto por defecto de Vite
   }
 })
